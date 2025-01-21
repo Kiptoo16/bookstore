47f0d48e78a3e52e0a5e373b0a6f8e289473b059
Kevin Kiptoo <kevin.langat@student.moringaschool.com>
2025-01-21 13:16:48 +0300

    Adds main.jsx

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+//Manage State with Redux (redux/store.js)//
+
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
